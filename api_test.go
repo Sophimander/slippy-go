787aaa6a5d3cf4561dd046a60d6f1282c918aa79
Sophia Moore <sophimander@gmail.com>
2025-04-25 18:01:21 -0400

    moved test data to test file

--- api_test.go
+++ api_test.go
@@ -5,10 +5,24 @@ import (
 	"testing"
 )
 
+var TimeData = []string{
+	"MORS#762",
+	"XATU#0",
+	"RON#404",
+	"ANZ#139",
+	"SHOOP#0",
+	"POLY#832",
+	"GOOPY#1",
+	"BEL#306",
+	"JEO#807",
+	"SO#0",
+	"NAT#4713",
+}
+
 func TestMain(t *testing.T) {
 	fmt.Println("Main Test")
 
-    client := NewClient()
+	client := NewClient()
 	for _, cc := range TimeData {
 		sUser, err := client.Run(cc)
 		if err != nil {
